641f623ca7601f02c75fa8c1c8447ae1e3feaf12
deltadecay <74830817+deltadecay@users.noreply.github.com>
2024-05-03 10:25:39 +0200

    tags for dense nodes

--- fileblock.go
+++ fileblock.go
@@ -56,10 +56,10 @@ func (fileBlock *FileBlock) ParseOSMData() {
 		log.Fatalln("Failed to parse primitive block in OSMData:", err)
 	}
 	log.Println("OSMData ------------------------")
-	log.Println("DateGranularity =", primitiveBlock.GetDateGranularity())
-	log.Println("Granularity =", primitiveBlock.GetGranularity())
-	log.Println("LatOffset =", primitiveBlock.GetLatOffset())
-	log.Println("LonOffset =", primitiveBlock.GetLonOffset())
+	//log.Println("DateGranularity =", primitiveBlock.GetDateGranularity())
+	//log.Println("Granularity =", primitiveBlock.GetGranularity())
+	//log.Println("LatOffset =", primitiveBlock.GetLatOffset())
+	//log.Println("LonOffset =", primitiveBlock.GetLonOffset())
 
 	granularity := int64(primitiveBlock.GetGranularity())
 	latoffset := primitiveBlock.GetLatOffset()
@@ -67,19 +67,52 @@ func (fileBlock *FileBlock) ParseOSMData() {
 
 	primitiveGroups := primitiveBlock.GetPrimitivegroup()
 
+	stringTable := primitiveBlock.GetStringtable().GetS()
+
 	for _, pg := range primitiveGroups {
+		nodes := pg.GetNodes()
+		ways := pg.GetWays()
+		relations := pg.GetRelations()
 		denseNodes := pg.GetDense()
 		log.Println("densenodes = ", len(denseNodes.GetId()))
+		log.Println("nodes = ", len(nodes))
+		log.Println("ways = ", len(ways))
+		log.Println("relations = ", len(relations))
 		var id, lat, lon int64
+
+		//log.Print("ids=", len(denseNodes.GetId()))
+		//log.Print("keysvals=", len(denseNodes.GetKeysVals()))
+		var tagIdx int = 0
 		for i, deltaId := range denseNodes.GetId() {
+
 			id += deltaId
+
 			lat += denseNodes.GetLat()[i]
 			lon += denseNodes.GetLon()[i]
 
 			latitude := 1e-9 * float64(latoffset+(granularity*lat))
 			longitude := 1e-9 * float64(lonoffset+(granularity*lon))
+			_, _ = latitude, longitude
 
-			log.Printf("Dense node id=%d lat=%f lon=%f", id, latitude, longitude)
+			//log.Printf("Dense node id=%d lat=%f lon=%f", id, latitude, longitude)
+			ti := tagIdx
+			for {
+				if ti == len(denseNodes.GetKeysVals()) {
+					break
+				}
+				keyIdx := denseNodes.GetKeysVals()[ti]
+				if keyIdx == 0 {
+					ti++
+					break
+				}
+				valIdx := denseNodes.GetKeysVals()[ti+1]
+
+				tagName := string(stringTable[keyIdx])
+				tagValue := string(stringTable[valIdx])
+				log.Printf("tag%d (%s=%s)", i, tagName, tagValue)
+				ti += 2
+			}
+			tagIdx = ti
 		}
 
 	}

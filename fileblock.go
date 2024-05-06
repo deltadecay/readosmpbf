32e763b290d52f63d0c712af875d2496d202c7a6
deltadecay <74830817+deltadecay@users.noreply.github.com>
2024-05-06 11:27:52 +0200

    parse nodes, ways, relations

--- fileblock.go
+++ fileblock.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/deltadecay/readosmpbf/pb"
 	"google.golang.org/protobuf/proto"
@@ -49,6 +51,154 @@ func (fileBlock *FileBlock) ParseOSMHeader() {
 	log.Println()
 }
 
+func parseDenseNodes(primitiveBlock *pb.PrimitiveBlock, denseNodes *pb.DenseNodes) {
+
+	granularity := int64(primitiveBlock.GetGranularity())
+	latoffset := primitiveBlock.GetLatOffset()
+	lonoffset := primitiveBlock.GetLonOffset()
+	stringTable := primitiveBlock.GetStringtable().GetS()
+
+	var id, lat, lon int64
+
+	//log.Print("ids=", len(denseNodes.GetId()))
+	//log.Print("keysvals=", len(denseNodes.GetKeysVals()))
+	var tagIdx int = 0
+	for i, deltaId := range denseNodes.GetId() {
+
+		id += deltaId
+
+		lat += denseNodes.GetLat()[i]
+		lon += denseNodes.GetLon()[i]
+
+		latitude := 1e-9 * float64(latoffset+(granularity*lat))
+		longitude := 1e-9 * float64(lonoffset+(granularity*lon))
+
+		tagsStr := ""
+		ti := tagIdx
+		for {
+			if ti == len(denseNodes.GetKeysVals()) {
+				break
+			}
+			keyIdx := denseNodes.GetKeysVals()[ti]
+			if keyIdx == 0 {
+				ti++
+				break
+			}
+			valIdx := denseNodes.GetKeysVals()[ti+1]
+
+			tagName := string(stringTable[keyIdx])
+			tagValue := string(stringTable[valIdx])
+			tagsStr += fmt.Sprintf("%s=%s|", tagName, tagValue)
+			ti += 2
+		}
+		tagIdx = ti
+
+		tagsStr = strings.TrimSuffix(tagsStr, "|")
+		log.Printf("Dense node id=%d lat=%f lon=%f tags=[%s]", id, latitude, longitude, tagsStr)
+	}
+}
+
+func parseNodes(primitiveBlock *pb.PrimitiveBlock, nodes []*pb.Node) {
+	granularity := int64(primitiveBlock.GetGranularity())
+	latoffset := primitiveBlock.GetLatOffset()
+	lonoffset := primitiveBlock.GetLonOffset()
+	stringTable := primitiveBlock.GetStringtable().GetS()
+
+	for _, node := range nodes {
+
+		id := node.GetId()
+		lat := node.GetLat()
+		lon := node.GetLon()
+		latitude := 1e-9 * float64(latoffset+(granularity*lat))
+		longitude := 1e-9 * float64(lonoffset+(granularity*lon))
+
+		tagsStr := ""
+		for ti := 0; ti < len(node.GetKeys()); ti++ {
+			keyIdx := node.GetKeys()[ti]
+			valIdx := node.GetVals()[ti]
+			tagName := string(stringTable[keyIdx])
+			tagValue := string(stringTable[valIdx])
+			tagsStr += fmt.Sprintf("%s=%s|", tagName, tagValue)
+		}
+		tagsStr = strings.TrimSuffix(tagsStr, "|")
+		log.Printf("Node id=%d lat=%f lon=%f tags=[%s]", id, latitude, longitude, tagsStr)
+	}
+}
+
+func parseWays(primitiveBlock *pb.PrimitiveBlock, ways []*pb.Way) {
+	//granularity := int64(primitiveBlock.GetGranularity())
+	//latoffset := primitiveBlock.GetLatOffset()
+	//lonoffset := primitiveBlock.GetLonOffset()
+	stringTable := primitiveBlock.GetStringtable().GetS()
+
+	for _, way := range ways {
+
+		id := way.GetId()
+		// References to nodes by their id
+		var ref int64
+		for _, deltaRef := range way.GetRefs() {
+			ref += deltaRef
+			// TODO lookup node with id == ref
+		}
+		// if optional_features has LocationsOnWays then ways have delta encoded coordinates
+		//lats := way.GetLat()
+		//lons := way.GetLon()
+		//latitude := 1e-9 * float64(latoffset+(granularity*lat))
+		//longitude := 1e-9 * float64(lonoffset+(granularity*lon))
+
+		tagsStr := ""
+		for ti := 0; ti < len(way.GetKeys()); ti++ {
+			keyIdx := way.GetKeys()[ti]
+			valIdx := way.GetVals()[ti]
+			tagName := string(stringTable[keyIdx])
+			tagValue := string(stringTable[valIdx])
+			tagsStr += fmt.Sprintf("%s=%s|", tagName, tagValue)
+		}
+		tagsStr = strings.TrimSuffix(tagsStr, "|")
+		log.Printf("Way id=%d tags=[%s]", id, tagsStr)
+	}
+}
+
+func parseRelations(primitiveBlock *pb.PrimitiveBlock, relations []*pb.Relation) {
+	//granularity := int64(primitiveBlock.GetGranularity())
+	//latoffset := primitiveBlock.GetLatOffset()
+	//lonoffset := primitiveBlock.GetLonOffset()
+	stringTable := primitiveBlock.GetStringtable().GetS()
+
+	for _, relation := range relations {
+
+		id := relation.GetId()
+
+		var memId int64
+		for mi, deltaMemId := range relation.GetMemids() {
+			memId += deltaMemId
+			// TODO lookup node/way/relation with memId
+			role := string(stringTable[relation.GetRolesSid()[mi]])
+			typ := relation.GetTypes()[mi]
+			if typ == pb.Relation_NODE {
+				// lookup node with memid
+			} else if typ == pb.Relation_WAY {
+				// lookup way with memid
+			} else if typ == pb.Relation_RELATION {
+				// lookup way with memid
+			}
+			log.Printf("\tmember type=%d, memid=%d, role=%s", typ, memId, role)
+
+		}
+
+		tagsStr := ""
+		for ti := 0; ti < len(relation.GetKeys()); ti++ {
+			keyIdx := relation.GetKeys()[ti]
+			valIdx := relation.GetVals()[ti]
+			tagName := string(stringTable[keyIdx])
+			tagValue := string(stringTable[valIdx])
+			tagsStr += fmt.Sprintf("%s=%s|", tagName, tagValue)
+		}
+		tagsStr = strings.TrimSuffix(tagsStr, "|")
+		log.Printf("Relation id=%d tags=[%s]", id, tagsStr)
+	}
+}
+
 func (fileBlock *FileBlock) ParseOSMData() {
 
 	primitiveBlock := &pb.PrimitiveBlock{}
@@ -61,13 +211,7 @@ func (fileBlock *FileBlock) ParseOSMData() {
 	//log.Println("LatOffset =", primitiveBlock.GetLatOffset())
 	//log.Println("LonOffset =", primitiveBlock.GetLonOffset())
 
-	granularity := int64(primitiveBlock.GetGranularity())
-	latoffset := primitiveBlock.GetLatOffset()
-	lonoffset := primitiveBlock.GetLonOffset()
-
 	primitiveGroups := primitiveBlock.GetPrimitivegroup()
-
-	stringTable := primitiveBlock.GetStringtable().GetS()
 
 	for _, pg := range primitiveGroups {
 		nodes := pg.GetNodes()
@@ -78,47 +222,16 @@ func (fileBlock *FileBlock) ParseOSMData() {
 		log.Println("nodes = ", len(nodes))
 		log.Println("ways = ", len(ways))
 		log.Println("relations = ", len(relations))
-		var id, lat, lon int64
 
-		//log.Print("ids=", len(denseNodes.GetId()))
-		//log.Print("keysvals=", len(denseNodes.GetKeysVals()))
-		var tagIdx int = 0
-		for i, deltaId := range denseNodes.GetId() {
-
-			id += deltaId
-
-			lat += denseNodes.GetLat()[i]
-			lon += denseNodes.GetLon()[i]
-
-			latitude := 1e-9 * float64(latoffset+(granularity*lat))
-			longitude := 1e-9 * float64(lonoffset+(granularity*lon))
-			_, _ = latitude, longitude
-
-			//log.Printf("Dense node id=%d lat=%f lon=%f", id, latitude, longitude)
-			ti := tagIdx
-			for {
-				if ti == len(denseNodes.GetKeysVals()) {
-					break
-				}
-				keyIdx := denseNodes.GetKeysVals()[ti]
-				if keyIdx == 0 {
-					ti++
-					break
-				}
-				valIdx := denseNodes.GetKeysVals()[ti+1]
-
-				tagName := string(stringTable[keyIdx])
-				tagValue := string(stringTable[valIdx])
-				log.Printf("tag%d (%s=%s)", i, tagName, tagValue)
-				ti += 2
-			}
-			tagIdx = ti
+		if len(pg.GetDense().GetId()) > 0 {
+			parseDenseNodes(primitiveBlock, pg.GetDense())
+		} else if len(pg.GetNodes()) > 0 {
+			parseNodes(primitiveBlock, pg.GetNodes())
+		} else if len(pg.GetWays()) > 0 {
+			parseWays(primitiveBlock, pg.GetWays())
+		} else if len(pg.GetRelations()) > 0 {
+			parseRelations(primitiveBlock, pg.GetRelations())
 		}
-
 	}
 
-	//stringTable := primitiveBlock.GetStringtable()
-	/*for _, s := range stringTable.GetS() {
-		log.Println(string(s))
-	}*/
 }
